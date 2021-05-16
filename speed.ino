bd0b4872853870cdad25f59cb0d0b40ef150c2eb
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-05-16 14:39:55 +0200

    Update speed.ino

--- speed.ino
+++ speed.ino
@@ -1,8 +1,8 @@
-// DIY BiCycle Speedometer 
-// distance in meter and speed in kilometer/hour 
+// DIY BiCycle Speedometer
+// distance in meter and speed in kilometer/hour
 // displays a * when magnet on Hall sensor and displays wheelcount
-// arduino nano 
-// hall sensor 
+// arduino nano
+// hall sensor
 // oled display
 // https://www.instructables.com/DIY-Cycle-Speedometer/
 
@@ -17,11 +17,9 @@
 
 
 #define pi 3.14
-#define diameter .71 // wheel diameter in meters
+#define diameter .71                 // wheel diameter in meters .71m = 71cm = 710mm
 #define numberofreadings 6
-#define cutoff 3
-#define velocitycutoff 1.2   // in m/s
-#define displaychangeafter 5000
+
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
@@ -54,42 +52,42 @@ void setup()
 
 
 void loop()
-{   
-    if (wheelcount == lastwheelcount) {
-      if (millis() - starttime >= 10000) {
-        velocity = 0;                       // if for 10 seonds no wheelcount set speed to zero 0 km/h
-      }
-    } else {
-      starttime = millis();
-      lastwheelcount = wheelcount;
+{
+  if (wheelcount == lastwheelcount) {
+    if (millis() - starttime >= 10000) {
+      velocity = 0;                       // if for 10 seonds no wheelcount set speed to zero 0 km/h
     }
+  } else {
+    starttime = millis();
+    lastwheelcount = wheelcount;
+  }
 
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setCursor(0, 0);
-    display.print(velocity * 18 / 5, 1);
-    display.setCursor(85, 0);
-    display.print("km");
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setCursor(0, 0);
+  display.print(velocity * 18 / 5, 1);
+  display.setCursor(85, 0);
+  display.print("km");
 
-    display.setTextSize(1);
-    display.setCursor(0, 25);
-    display.print(distance, 0);
-    display.print(" m");
+  display.setTextSize(1);
+  display.setCursor(0, 25);
+  display.print(distance, 0);
+  display.print(" m");
 
-    display.setTextSize(1);
+  display.setTextSize(1);
 
-    if (digitalRead(2)) {
-      display.setCursor(75, 25);
-      display.print(" ");
-    } else {
-      display.setCursor(75, 25);
-      display.print("*");
-    }
-    
-    display.setCursor(85, 25);
-    display.print(wheelcount);
-    
-    display.display();
+  if (digitalRead(2)) {
+    display.setCursor(75, 25);
+    display.print(" ");
+  } else {
+    display.setCursor(75, 25);
+    display.print("*");
+  }
+
+  display.setCursor(85, 25);
+  display.print(wheelcount);
+
+  display.display();
 }
 
 
@@ -106,8 +104,8 @@ void cross()
   if (wheelcount > 2)
   {
     distance = wheelcount * diameter * pi;
-    velocity = diameter * pi / (instant - previnstant) ;
-    velocity = velocity * 1000;
+    velocity = (diameter * pi / (instant - previnstant))  * 1000;
+
   }
 
 }
